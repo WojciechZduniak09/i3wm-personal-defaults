c581e1ac0371e7619d26c05b74d91a721094232a
WojciechZduniak09 <151409152+WojciechZduniak09@users.noreply.github.com>
2024-12-27 21:38:43 +0100

    Fixed unzip installation

--- install.sh
+++ install.sh
@@ -195,7 +195,7 @@ install_reverse_engineering_packages() {
 			echo 100
 			return
 		fi
-		sudo apt install unzip -y
+		sudo apt install unzip -y 2> /dev/null
 		rm -rf "$HOME"/Programs/ghidra_11.2.1_PUBLIC
 		local GHIDRA_ZIP_PATH
 		GHIDRA_ZIP_PATH="$HOME/Programs/ghidra_11.2.1_PUBLIC_20241105.zip"
